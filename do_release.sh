1540d139f490efa18261b3608bb7818cb6259a2e
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2015-02-06 13:56:30 +0100

    Initial debian packaging stuff
    fixes #17, but documentation is needed

--- do_release.sh
+++ do_release.sh
@@ -7,15 +7,26 @@ fi
 
 VERSION=$1
 
+echo "# I'm not running anything, run this by yourself"
 
-echo "I'm not running anything, run this by yourself"
+last=`git tag -l|tail -n1`
 
 echo git-dch \
     --release \
     --new-version=$VERSION \
+    --since=\"$last\" \
+    --commit \
     --verbose 
 
 echo git tag -s $VERSION
 
-
 echo python setup.py sdist upload
+
+echo mkdir /tmp/BUILD
+
+echo
+echo "# Debian packages"
+echo cp dist/caso-${VERSION}.tar.gz /tmp/BUILD/caso_${VERSION}.orig.tar.gz
+echo cd /tmp/BUILD
+echo tar zxfv caso_${VERSION}.orig.tar.gz
+echo debuild -S -sa
